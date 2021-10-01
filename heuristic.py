2fa8ad9acc07784595769ce1d3cc5b268e46e7fc
Rhodri Cusack <cusackrh@kelvin01.cluster>
2021-10-01 12:27:49 +0100

    first commit

--- heuristic.py
+++ heuristic.py
@@ -1,4 +1,6 @@
 import os
+from os import path
+
 def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
         raise ValueError('Template must be a valid format string')
@@ -11,18 +13,47 @@ def infotodict(seqinfo):
     seqitem: run number during scanning
     subindex: sub index within group
     """
-    t1w = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_run-00{item:01d}_T1w')
-    func_video = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-video_run-00{item:01d}_bold')
-    func_pictures = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-pictures_run-00{item:01d}_bold')
-
-    info = {t1w: [], func_video: [], func_pictures: []}
+    anat = create_key('anat/sub-{subject}_{session}_run-00{item:01d}_T1w')
+    func_video = create_key('func/sub-{subject}_{session}_dir-AP_task-video_run-00{item:01d}_bold')
+    func_pictures = create_key('func/sub-{subject}_{session}_dir-AP_task-pictures_run-00{item:01d}_bold')
+    func_rest5 = create_key('func/sub-{subject}_{session}_dir-AP_task-rest5_run-00{item:01d}_bold')
+    func_rest10 = create_key('func/sub-{subject}_{session}_dir-AP_task-rest10_run-00{item:01d}_bold')
+    dwi = create_key('dwi/sub-{subject}_{session}_dir-{dir}_run-{item}_dwi')
+    fmap = create_key('fmap/sub-{subject}_{session}_dir-{dir}_run-{item}_epi')
+    info = {anat: [], func_video: [], func_pictures: [], func_rest5: [], func_rest10: [], dwi: [], fmap:[] }
     
     for idx, s in enumerate(seqinfo):
-        if (s.dim1 == 256) and (s.dim2 == 248) and ('t1_mprage_sag_p2' in s.protocol_name):
-            info[t1w].append(s.series_id)
-        if (s.dim1 == 70) and (s.dim2 == 64) and ('ep2d_bold_moco_s4_post_ch64_VIDEO' in s.protocol_name):
+        if 't2_tse_tra_p3_noisereduction' in s.protocol_name:
+            info[anat].append(s.series_id)
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_videos_AP' in s.protocol_name):
             info[func_video].append(s.series_id)
-        if (s.dim1 == 70) and (s.dim2 == 64) and ('ep2d_bold_moco_s4_post_ch64_Pictures' in s.protocol_name):
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_Pictures_AP' in s.protocol_name):
             info[func_pictures].append(s.series_id)
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_Resting_10' in s.protocol_name):
+            info[func_rest10].append(s.series_id)
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_bold_Resting_5' in s.protocol_name):
+            info[func_rest5].append(s.series_id)
+        if (s.dim1 == 96) and (s.dim2 == 96) and ('cmrr_mbep2d_diff_AP_mb4_norm_pulse1.5x' in s.protocol_name):
+            info[dwi].append({'item': s.series_id, 'dir':'AP'})
+        if (s.dim1 == 96) and (s.dim2 == 96) and ('cmrr_mbep2d_diff_PA_mb4_norm_pulse1.5x' in s.protocol_name):
+            info[dwi].append({'item': s.series_id, 'dir':'PA'})
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_se_AP' in s.protocol_name):
+            info[fmap].append({'item': s.series_id, 'dir':'AP'})
+        if (s.dim1 == 64) and (s.dim2 == 64) and ('cmrr_mbep2d_se_PA' in s.protocol_name):
+            info[fmap].append({'item': s.series_id, 'dir':'PA'})
+
+    print('*** infotodict')
+    print(info)
 
     return info
+
+# Get a numeric subject ID for BIDS format from list of DICOM file names
+def infotoids(seqinfos, outdir):
+    # Use the sub and ses labels as given in the output path
+    fp = outdir.split(path.sep)
+    if fp[-1][:4] == 'ses-':
+        ses=fp[-1][4:]
+    if fp[-2][:4] == 'sub-':
+        sub=fp[-2][4:]
+    
+    return({'subject':sub, 'session':ses, 'locator':outdir})
